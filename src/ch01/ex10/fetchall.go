7991aa0a2e49eb1fc38442353ebb0e144d889a22
akeboshi <akari.akeboshi@gmail.com>
2016-02-20 10:36:15 +0900

    add ch01/ex10, ch01/ex11

---
+++ src/ch01/ex10/fetchall.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	splitURL := strings.Split(url, "/")
+	filename := splitURL[2]
+	file, err := os.Create(filename)
+	if err != nil {
+		ch <- fmt.Sprintf("file open err %s: %v", filename, err)
+		return
+	}
+	defer file.Close()
+
+	nbytes, err := io.Copy(file, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
---
+++ src/ch01/ex11/result.md
@@ -0,0 +1,20 @@
+演習問題11
+=========
+* より長い引数リストでfetchallを実行した結果
+```
+./fetchall http://google.com http://facebook.co
+m http://youtube.com http://yahoo.com http://amazon.com http://wikipedia
+.org http://google.co.in http://twitter.com http://live.com http://notre
+sponse.site
+Get http://notresponse.site: dial tcp: lookup notresponse.site: no such hostdd
+0.32s   21280 http://google.co.in
+0.33s   19412 http://google.com
+1.47s  251390 http://twitter.com
+1.76s  393937 http://youtube.com
+1.96s   54537 http://wikipedia.orgdddd
+2.10s  411081 http://yahoo.com
+2.48s  380514 http://amazon.com
+2.62s    9656 http://live.com
+2.87s   72796 http://facebook.com
+2.87s elapsed
+```
