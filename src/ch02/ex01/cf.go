ae283c7c8336b9dfc91926c8609ac72ddb9e5402
akeboshi <akari.akeboshi@gmail.com>
2016-02-20 17:51:14 +0900

    add ch02/ex01

---
+++ src/ch02/ex01/cf.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"ch02/ex01/tempconv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		k := tempconv.Kelvin(t)
+		fmt.Printf("%s = %s = %s\n", f, tempconv.FToC(f), tempconv.FToK(f))
+		fmt.Printf("%s = %s = %s\n", c, tempconv.CToF(c), tempconv.CToK(c))
+		fmt.Printf("%s = %s = %s\n", k, tempconv.Kelvin(k), tempconv.KToF(k))
+	}
+}
---
+++ src/ch02/ex01/tempconv/conv.go
@@ -0,0 +1,19 @@
+package tempconv
+
+// CToF は摂氏の温度を華氏へ変換します
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// CToK は摂氏の温度を絶対温度へ変換します
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// FToC は華氏の温度を摂氏へ変換します
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// FToK は華氏の温度を絶対温度へ変換します
+func FToK(f Fahrenheit) Kelvin { return Kelvin(CToK(FToC(f))) }
+
+// KToC は絶対温度を摂氏の温度へ変換します
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// KToF は絶対温度を華氏の温度へ変換します
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(CToF(KToC(k))) }
