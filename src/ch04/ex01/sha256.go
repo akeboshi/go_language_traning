53aa5849d82ff63f34630a2b0b9816f2733e6b0a
akeboshi <akari.akeboshi@gmail.com>
2016-03-22 07:51:56 +0900

    github.com ignored. add ch04/ex01

---
+++ src/ch04/ex01/sha256.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"ch02/ex03/popcount"
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x diff %x = %d\n", c1, c2, popDifCount(c1, c2))
+}
+
+func popDifCount(x, y [32]byte) int {
+	var count int
+	for i := 0; i < 32; i++ {
+		count += popcount.PopCountHackers(uint64(x[i] ^ y[i]))
+	}
+	return count
+}
