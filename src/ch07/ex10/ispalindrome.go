1078f41e649728b5628f8b5a20828c4c48297bbe
akeboshi <akari.akeboshi@gmail.com>
2016-06-07 21:59:01 +0900

    add ex12,ex11

---
+++ src/ch07/ex10/ispalindrome.go
@@ -0,0 +1,21 @@
+package main
+
+import "sort"
+
+func main() {
+	list := sort.StringSlice{"a", "b"}
+	println(IsPalindrome(list))
+	list = sort.StringSlice{"abc", "foo", "foo", "abc"}
+	println(IsPalindrome(list))
+}
+
+func IsPalindrome(s sort.Interface) bool {
+	len := s.Len()
+	for i := 0; i < len/2; i++ {
+		j := len - i - 1
+		if s.Less(i, j) || s.Less(j, i) {
+			return false
+		}
+	}
+	return true
+}
---
+++ src/ch07/ex11/database.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	mux := http.NewServeMux()
+	mux.Handle("/create", http.HandlerFunc(db.create))
+	mux.Handle("/delete", http.HandlerFunc(db.delete))
+	mux.Handle("/update", http.HandlerFunc(db.update))
+	mux.Handle("/list", http.HandlerFunc(db.list))
+	mux.Handle("/price", http.HandlerFunc(db.price))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+}
+
+type dollars float32
+type database map[string]dollars
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+// POST であるべき
+func (db database) create(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if item == "" || price == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "あいてむとねだんをしていしてね")
+	}
+	_, ok := db[item]
+	if ok {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "していされたあいてむはもうそんざいしてるよ")
+	}
+	intPrice, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "ねだんはすうじでしていしてね")
+	}
+	db[item] = dollars(intPrice)
+	fmt.Fprintf(w, "%s を %s でとうろくしたよ！", item, db[item])
+}
+
+// DELETE
+func (db database) delete(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	if item == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "さくじょするあいてむをえらんでね")
+		return
+	}
+	_, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s はとうろくされてないよ！\n", item)
+		return
+	}
+	delete(db, item)
+	fmt.Fprintf(w, "%s のさくじょにせいこう！", item)
+}
+
+// PUT
+func (db database) update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+	if item == "" || price == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "あいてむとねだんをしていしてね")
+		return
+	}
+	intPrice, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "ねだんはすうじでしていしてね")
+		return
+	}
+	db[item] = dollars(intPrice)
+	fmt.Fprintf(w, "%s を %s でこうしんしたよ！", item, db[item])
+}
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "%s はとうろくされてないよ！\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
