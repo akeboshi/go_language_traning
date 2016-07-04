c8195cb8001367ead1e6f1ff510883cbe92e5244
akeboshi <akari.akeboshi@gmail.com>
2016-07-05 08:51:17 +0900

    add ch08-ex12

---
+++ src/ch08/ex12/chat.go
@@ -0,0 +1,93 @@
+// Copyright (c) 2016 by akeboshi. All Rights Reserved.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go broadcaster()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
+
+type client struct {
+	ch   chan<- string
+	name string
+}
+
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string)
+)
+
+func broadcaster() {
+	clients := make(map[client]bool)
+	for {
+		select {
+		case msg := <-messages:
+			for cli := range clients {
+				cli.ch <- msg
+			}
+		case cli := <-entering:
+			var list []string
+			for c := range clients {
+				list = append(list, c.name)
+			}
+			clients[cli] = true
+			if len(list) > 0 {
+				str := "member list\n" +
+					"-----------\n" +
+					strings.Join(list, "\n") +
+					"\n-----------"
+				cli.ch <- str
+			}
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli.ch)
+		}
+	}
+}
+
+func handleConn(conn net.Conn) {
+	ch := make(chan string)
+	go clientWriter(conn, ch)
+
+	who := conn.RemoteAddr().String()
+	ch <- "You are " + who
+	messages <- who + " has arrived"
+	cli := client{ch, who}
+	entering <- cli
+
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		messages <- who + ": " + input.Text()
+	}
+
+	leaving <- cli
+	messages <- who + " has left"
+	conn.Close()
+}
+
+func clientWriter(conn net.Conn, ch <-chan string) {
+	for msg := range ch {
+		fmt.Fprintln(conn, msg)
+	}
+}
